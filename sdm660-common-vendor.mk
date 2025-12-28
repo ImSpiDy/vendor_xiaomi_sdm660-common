1edcbe1b03bf46ec354bccb2fe80616da574477e
ImSpiDy <SpiDy2713@gmail.com>
2025-12-28 20:56:06 +0100

    Revert "sdm660-common: Drop unused ConsumerIR blobs"
    This reverts commit dce5354d989f41cff9d27480f2c4df490a3e0a94.
    
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- sdm660-common-vendor.mk
+++ sdm660-common-vendor.mk
@@ -812,3 +812,10 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.fm@1.0 \
     vendor.qti.hardware.fm@1.0-impl
 endif
+
+ifeq ($(BOARD_HAVE_IR),true)
+
+PRODUCT_PACKAGES += \
+    consumerir.lirc.sdm660 \
+    consumerir.spi.sdm660
+endif
